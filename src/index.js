e34c25a8a00da0dfcbec6f32721a68de4ae21749
GStewartN <stewart.nathang@gmail.com>
2018-01-04 15:50:25 -0800

    Add babel, webpack.config.prod, hotloading, change file structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
 
-ReactDOM.render(React.createElement('h1', null, 'Hello from webpack!'), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+     <App/>,
+     document.getElementById('root')
+    );
+  });
+}
