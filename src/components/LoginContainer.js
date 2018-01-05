0cf7b0360256dd0c92d70157dfd5c43d13a7a5f3
GStewartN <stewart.nathang@gmail.com>
2018-01-05 12:47:37 -0800

    Move image to logincontainer, add form

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -4,7 +4,20 @@ class LoginContainer extends React.Component {
   render () {
     return (
       <div id="LoginContainer" className="inner-container">
-
+        <div id="Header">
+          <img src="/assets/icon.png" alt="logo" />
+          <h1>Say No More Fam</h1>
+        </div>
+        <form>
+          <p>Sign in or sign up by entering your email and password.</p>
+          <input
+            type="text"
+            placeholder="Your Email"/>
+          <input
+            type="password"
+            placeholder="Your Password"/>
+          <button className="red light" type="submit">Login</button>
+        </form>
       </div>
     );
   }
