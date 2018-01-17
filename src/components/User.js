1e6780426ba1308be14973a30560435623656431
GStewartN <stewart.nathang@gmail.com>
2018-01-17 11:28:34 -0800

    Add back to chat btn to user with link to home

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "./Header";
 
 class User extends React.Component {
   render() {
     return (
       <div id="UserContainer">
-        <Header />
+        <Header>
+          <Link to="/">
+            <button className="red">
+              Back to chat
+            </button>
+          </Link>
+        </Header>
         <h1>Hello from User for User {this.props.match.params.id}</h1>
       </div>
     );
