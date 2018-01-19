5f33fe82ad5a053b19d2032e74fde231a744f6c8
GStewartN <stewart.nathang@gmail.com>
2018-01-19 14:46:57 -0800

    Write event listener for database functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.sendNotifications = functions.database.ref('/messages/{messageId}').onWrite(event => {
+  const snapshot = event.data;
+  if (snapshot.previous.val()) {
+    return;
+  }
+  const payload = {
+    notification: {
+      title: `${snapshot.val().author}`,
+      body: `${snapshot.val().msg}`,
+      icon: 'assets/icon/png',
+      click_action: `https://${functions.config().firebase.authDomain}`
+    }
+  };
+});
